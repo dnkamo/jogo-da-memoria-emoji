59e9777dda86cf4401a510dfe405f537c771a442
dnkamo <149275743+dnkamo@users.noreply.github.com>
2023-12-22 20:45:20 -0300

    Add files via upload

---
+++ src/scripts/engine.js
@@ -0,0 +1,61 @@
+const emojis  = [
+    "🤔",
+    "🤔",
+    "😂",
+    "😂",
+    "🤦‍♂️",
+    "🤦‍♂️",
+    "✔",
+    "✔",
+    "😏",
+    "😏",
+    "😎",
+    "😎",
+    "🤨",
+    "🤨",
+    "😐",
+    "😐"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    let idBox = document.createAttribute('id');
+    idBox.value = i;
+    box.setAttributeNode(idBox);
+
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+      if (openCards[0].innerHTML === openCards[1].innerHTML && openCards[0].id !== openCards[1].id) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");        
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu!")
+    }
+}
